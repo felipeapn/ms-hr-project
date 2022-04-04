e6ccfef49d57d42b64c4cc3f49ee17b3932f8a3a
felipeapn <FN202123@%>
2022-04-04 08:43:22 -0300

    Implement hr-worker

---
+++ hr-worker/src/main/java/com/felipeapn/hrworker/entities/Worker.java
@@ -0,0 +1,23 @@
+package com.felipeapn.hrworker.entities;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "TB_WORKER")
+public class Worker implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+}
