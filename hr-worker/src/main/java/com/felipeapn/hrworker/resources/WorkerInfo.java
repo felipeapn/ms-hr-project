99063b72e3a32510d7107add303ca613b3c6f911
felipeapn <felipeapn@yahoo.com.br>
2022-05-30 12:36:39 -0300

    Config work info

---
+++ hr-worker/src/main/java/com/felipeapn/hrworker/resources/WorkerInfo.java
@@ -0,0 +1,31 @@
+package com.felipeapn.hrworker.resources;
+
+import com.felipeapn.hrworker.entities.Worker;
+import com.felipeapn.hrworker.repositories.WorkerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping( value = "/info")
+public class WorkerInfo {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping
+    public ResponseEntity<String> info() {
+
+        log.info("test.config=" + environment.getProperty("test.config"));
+
+        return ResponseEntity.ok("test.config=" +  environment.getProperty("test.config"));
+    }
+}
